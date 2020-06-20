1b046824101cb2676440acf1de497b113384638d
taka.s <takapie81@gmail.com>
2020-06-20 16:36:03 +0000

    add user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,18 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :destroy]
         
     def index
-        @tasks = Task.all
+        if logged_in?
+            @tasks = current_user.tasks
+        end
     end
     
     def show
+        @task = Task.find(params[:id])
+        if @task.user.id != current_user.id
+            redirect_to :tasks
+        end
     end
     
     def new
@@ -13,7 +20,7 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'Taskが正常に投稿されました'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+    belongs_to :user
+    
     validates :content, presence: true
     validates :status, presence: true, length: {maximum: 10}
 end
