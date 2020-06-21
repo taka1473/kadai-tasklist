919470071341547719365c794fc438d1b940cb50
taka.s <takapie81@gmail.com>
2020-06-21 10:13:26 +0000

    error correction

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,13 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
-    before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
         
     def index
-        if logged_in?
-            @tasks = current_user.tasks
-        end
+        @tasks = current_user.tasks
     end
     
     def show
         @task = Task.find(params[:id])
-        if @task.user.id != current_user.id
-            redirect_to :tasks
-        end
     end
     
     def new
@@ -55,11 +50,14 @@ class TasksController < ApplicationController
     
     private
     
-    def set_task
-        @task = Task.find(params[:id])
-    end
-    
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
     end
 end
